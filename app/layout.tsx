d033768d7c40451db43a2062a90d07223155fc21
gilbmporto <gilbmp@gmail.com>
2024-01-05 19:10:51 -0300

    Inserted the 'export const fetchCache' variable inside Layout.tsx one more time in order to make the app work as expected

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+// import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/Header"
 
-const inter = Inter({ subsets: ["latin"] })
+// const inter = Inter({ subsets: ["latin"] })
+
+export const fetchCache = "force-no-store"
 
 export const metadata: Metadata = {
   title: "Surf's Gym App",
