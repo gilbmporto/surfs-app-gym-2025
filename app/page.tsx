6ea341779ef0fea1c0e91b745de7c47162261a40
gilbmporto <gilbmp@gmail.com>
2024-01-05 16:40:34 -0300

    Changed main page component to handle the axios error and updated ReadME.md

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,34 @@ import axios from "axios"
 import { UserEventWithTrainingsProps } from "./api/users/route"
 
 export default async function Home() {
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_REACT_APP_API_URL!}api/users/`
-  )
-  const users: UserEventWithTrainingsProps[] = await response.data.data
+  const usersData = await getAllUsers()
+  const users: UserEventWithTrainingsProps[] = usersData || []
+
+  async function getAllUsers() {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_REACT_APP_API_URL!}api/users/`
+      )
+      const users: UserEventWithTrainingsProps[] = await response.data.data
+      return users
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <main className="flex flex-col items-center max-w-6xl mx-auto">
-      {users.map((user: UserEventWithTrainingsProps, index: number) => (
-        <UserData
-          key={index}
-          userId={user.userId}
-          userName={user.userName}
-          trainings={user.trainings}
-          timestamp={user.timestamp}
-          lastTrainingTimestamp={user.lastTrainingTimestamp}
-        />
-      ))}
+      {users.length > 0 &&
+        users.map((user: UserEventWithTrainingsProps, index: number) => (
+          <UserData
+            key={index}
+            userId={user.userId}
+            userName={user.userName}
+            trainings={user.trainings}
+            timestamp={user.timestamp}
+            lastTrainingTimestamp={user.lastTrainingTimestamp}
+          />
+        ))}
     </main>
   )
 }
