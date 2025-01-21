810ada5b1de4ee67426864ee5318c944a57eff04
gilbmporto <gilbmp@gmail.com>
2025-01-20 21:49:32 -0300

    Updated app title and .env variable at vercel

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,9 @@ import React from "react"
 export default function Header() {
   return (
     <>
-      <h1 className="text-5xl font-bold mb-10 text-center">Surf's Gym App</h1>
+      <h1 className="text-5xl font-bold mb-10 text-center">
+        Surf's Gym App 2025
+      </h1>
       <img
         src="terry-crews.jpg"
         alt="Surf's Gym SuperLogo"
